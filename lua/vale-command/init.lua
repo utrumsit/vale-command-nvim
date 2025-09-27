20be8641af8343cb4e81c9104ceb77b44b267537
utrumsit <byzkarl@yahoo.com>
2025-09-27 10:00:32 -0500

    Initial commit

---
+++ lua/vale-command/init.lua
@@ -0,0 +1,34 @@
+return {
+  setup = function()
+    vim.api.nvim_create_user_command("Vale", function()
+      local file = vim.api.nvim_buf_get_name(0)
+      local output = vim.fn.system("vale --output=JSON " .. file)
+      local results, err = vim.fn.json_decode(output)
+
+      if err then
+        print("Error parsing Vale output: " .. err)
+        return
+      end
+
+      local qf_list = {}
+      for filename, errors in pairs(results) do
+        for _, error in ipairs(errors) do
+          table.insert(qf_list, {
+            filename = filename,
+            lnum = error.Line,
+            col = error.Span[1],
+            text = error.Message,
+            type = string.sub(error.Severity, 1, 1):upper(),
+          })
+        end
+      end
+
+      if #qf_list > 0 then
+        vim.fn.setqflist(qf_list)
+        vim.cmd("copen")
+      else
+        print("No issues found by Vale.")
+      end
+    end, {})
+  end,
+}
---
+++ plugin/vale.lua
@@ -0,0 +1,6 @@
+return {
+  "karl-s/vale-command-nvim",
+  config = function()
+    require("vale-command").setup()
+  end,
+}
